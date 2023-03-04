10b2329b5779bccb94c768c5f6b66b6e50a7760c
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-03-04 09:04:49 -0500

    feat: added change tracking fields to viewmodel

---
+++ Workly.Core/ViewModels/CommitViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workly.Core.ViewModels
+{
+    public class CommitViewModel
+    {
+        public int? AddedFileChange { get; set; }
+        public int? EditedFileChange { get; set; }
+        public int? DeletedFileChange { get; set; }
+    }
+}
