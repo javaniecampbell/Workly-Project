a7a5a313e934879f3cfcf387ffbb5b416d55603c
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-03-05 09:59:05 -0500

    feat: added querystrings to links and descriptions

--- dashboardui/src/App.tsx
+++ dashboardui/src/App.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './assets/styles/App.css';
-import { Link, Outlet } from 'react-router-dom';
+import React from "react";
+import logo from "./logo.svg";
+import "./assets/styles/App.css";
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="">
       <header className="">
-              <h1>Main</h1>
-              <div>
-                  <Link to="/">Home</Link>
-                  <Link to="/contacts?success=true">True</Link>
-                  <Link to="/contacts?success=false">False</Link>
-              </div>
-              <Outlet/>
+        <h1>Main</h1>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            width: "100%",
+            justifyContent: "space-around",
+          }}
+        >
+          <Link to="/">Home</Link>
+          <Link to="/contacts?success=true">Contacts - Visible</Link>
+          <Link to="/contacts?success=false">Contacts - Hidden</Link>
+        </div>
+        <Outlet />
       </header>
     </div>
   );
