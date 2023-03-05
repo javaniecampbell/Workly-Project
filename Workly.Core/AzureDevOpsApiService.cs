ffda5e2cad49e881cc758a28f4f4453ab5d33376
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-03-05 10:07:53 -0500

    refactor: moved get connection and accepted personalAccessToken and organizationUrl as contstructor params

--- Workly.Core/AzureDevOpsApiService.cs
+++ Workly.Core/AzureDevOpsApiService.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Workly.Core
 {
-    internal class AzureDevOpsApiService
+    public class AzureDevOpsApiService
     {
+        private readonly string organizationUrl;
+        private readonly string personalAccessToken;
+
+        public AzureDevOpsApiService(string organizationUrl, string personalAccessToken)
+        {
+            this.organizationUrl = organizationUrl;
+            this.personalAccessToken = personalAccessToken;
+        }
+        private VssConnection GetVssConnection()
+        {
+            var basicCredential = new VssBasicCredential(string.Empty, personalAccessToken);
+            var orgUri = new Uri(organizationUrl);
+            VssConnection connection = new VssConnection(orgUri, basicCredential);
+            return connection;
+        }
     }
 }
