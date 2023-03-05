e193e368b9a13e63a2ea063108e1d1b66be5a45b
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-03-05 10:13:37 -0500

    refactor: moved GetAllCommits and made it public in the service

--- Workly.Core/AzureDevOpsApiService.cs
+++ Workly.Core/AzureDevOpsApiService.cs
@@ -22,29 +22,44 @@ namespace Workly.Core
             this.personalAccessToken = personalAccessToken;
         }
 
-        private async IAsyncEnumerable<Commit> TransformToModel(List<GitCommitRef> commits, string repo)
+        public async IAsyncEnumerable<Commit> GetAllCommits()
+        {
+            string[] repos = new string[] {
+                "dc1e47d0-4b54-4bec-a60c-6dd6ef15df91",
+                "946fd4ff-c9b0-43ff-bc8e-9ee68b2dcc6f",
+                "fee96637-9fc7-428c-8d25-bd50d3961325",
+                "9993424a-e46b-47d7-a00c-f128402cbb71",
+                "0970eff7-d8ea-4678-b02d-97c7486802aa"
+            };
+            var connection = GetVssConnection();
+            using (var git = await connection.GetClientAsync<GitHttpClient>())
+            {
+                foreach (var repo in repos)
+                {
+                    var commits = await git.GetCommitsAsync("40ef9a6b-d17b-4454-b6a6-3de33e04504f", repo, new GitQueryCommitsCriteria
+                    {
+                        Top = Int32.MaxValue,
+                    });
+
+                    await foreach (Commit commit in TransformToModel(commits, repo))
+                    {
+                        yield return commit;
+                    }
+
+                }
+            }
+        }
+
+        public async IAsyncEnumerable<Commit> TransformToModel(List<GitCommitRef> commits, string repo)
         {
             var connection = GetVssConnection();
-            // var result = new List<Commit>();
             using (var client = await connection.GetClientAsync<GitHttpClient>())
             {
                 foreach (var commitRef in commits)
                 {
                     var commit = await client.GetCommitAsync(commitRef.CommitId, repo);
                     var repository = await client.GetRepositoryAsync(repo);
-                    //result.Add(new Commit
-                    //{
-                    //    CommitId = commitRef.CommitId,
-                    //    AuthorName = commitRef.Author.Name,
-                    //    AuthorEmail = commitRef.Author.Email,
-                    //    AuthorDate = commitRef.Author.Date,
-                    //    CommitterName = commitRef.Committer.Name,
-                    //    Subject = commitRef.Comment.Split("\n")[0],
-                    //    CommitterEmail = commitRef.Committer.Email,
-                    //    CommitterDate = commitRef.Committer.Date,
-                    //    Body = commit.Comment,
-                    //    CommitMessage = commitRef.Comment,
-                    //});
+                   
                     yield return new Commit
                     {
                         CommitId = commitRef.CommitId,
@@ -65,9 +80,6 @@ namespace Workly.Core
                     };
                 }
             }
-
-
-            //return result;
         }
         private VssConnection GetVssConnection()
         {
