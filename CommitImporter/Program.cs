c9807fac0b2ae06e0d87e8cf128eca6cb1971966
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-03-05 10:31:24 -0500

    refactor: replaced consumer of the api to core service apis and repositories then archived the scatch pad implementation

--- CommitImporter/Program.cs
+++ CommitImporter/Program.cs
@@ -12,6 +12,36 @@ namespace CommitImporter
     {
         static async Task Main(string[] args)
         {
+            var azureDevOpService = new AzureDevOpsApiService(Resources.AZURE_DEVOPS_ORGANIZATION_URL, Resources.AZURE_DEVOPS_PAT);
+            var commitsRepository = new CommitsRepository();
+            var stopwatch = new Stopwatch();
+            string[] repositories = new string[] {
+                "dc1e47d0-4b54-4bec-a60c-6dd6ef15df91",
+                "946fd4ff-c9b0-43ff-bc8e-9ee68b2dcc6f",
+                "fee96637-9fc7-428c-8d25-bd50d3961325",
+                "9993424a-e46b-47d7-a00c-f128402cbb71",
+                "0970eff7-d8ea-4678-b02d-97c7486802aa"
+            };
+
+            var projectId = "40ef9a6b-d17b-4454-b6a6-3de33e04504f";
+
+            Console.WriteLine("Retrieving all commits.....");
+            stopwatch.Start();
+            var commits = await azureDevOpService.GetAllCommits(projectId,repositories).ToListAsync();
+            stopwatch.Stop();
+            Console.WriteLine("Time taken for result: {0} seconds", stopwatch.ElapsedMilliseconds / 1000f);
+            
+            Console.WriteLine("Saving all commits.....");
+            stopwatch.Restart();
+            _ = await commitsRepository.BulkInsertAsync(commits);
+            stopwatch.Stop();
+            Console.WriteLine("Time taken for result: {0} seconds", stopwatch.ElapsedMilliseconds / 1000f);
+
+        }
+        #region Old Implementation for Reference
+
+        private static async Task ArchiveAfterRefactor()
+        {
             try
             {
                 var stopwatch = new Stopwatch();
@@ -37,7 +67,7 @@ namespace CommitImporter
 
                 var repo = new CommitsRepository();
                 Console.WriteLine("Saving all commits.....");
-                
+
                 stopwatch.Restart();
                 _ = await repo.BulkInsertAsync(commits);
                 stopwatch.Stop();
@@ -86,7 +116,7 @@ namespace CommitImporter
         {
             var connection = GetVssConnection();
             // var result = new List<Commit>();
-             using (var client = await connection.GetClientAsync<GitHttpClient>())
+            using (var client = await connection.GetClientAsync<GitHttpClient>())
             {
                 foreach (var commitRef in commits)
                 {
@@ -113,7 +143,7 @@ namespace CommitImporter
                         AuthorDate = commitRef.Author.Date,
                         CommitterName = commitRef.Committer.Name.Equals("=") ? commit.Push.PushedBy.DisplayName + " *MP" : commitRef.Committer.Name,
                         Subject = commitRef.Comment.Split("\n")[0],
-                        CommitterEmail = commitRef.Committer.Email.Equals("=") ? commit.Push.PushedBy.UniqueName + " *MP"  : commitRef.Committer.Email,
+                        CommitterEmail = commitRef.Committer.Email.Equals("=") ? commit.Push.PushedBy.UniqueName + " *MP" : commitRef.Committer.Email,
                         CommitterDate = commitRef.Committer.Date,
                         Body = commit.Comment,
                         CommitMessage = commitRef.Comment,
@@ -136,6 +166,7 @@ namespace CommitImporter
             var orgUri = new Uri(Resources.AZURE_DEVOPS_ORGANIZATION_URL);
             VssConnection connection = new VssConnection(orgUri, basicCredential);
             return connection;
-        }
+        } 
+        #endregion
     }
 }
